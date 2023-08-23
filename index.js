89c32ed4ce8bfeace5db66db04f71d8fd717b859
Stoic-Coder2003 <shubhmehrotra.personal@gmail.com>
2023-08-23 21:27:08 +0530

    added elements to the portfolio

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ var image = document.getElementById('moving-image');
 image.addEventListener('click', function() {
     image.classList.toggle('move');
 });
+
+const motionObject = document.getElementById('pointing');
+const triggerArea = document.getElementById('trigger-area');
+
+triggerArea.addEventListener('mouseenter', () => {
+    motionObject.style.animationPlayState = 'running'; // Start the animation
+});
+
+triggerArea.addEventListener('mouseleave', () => {
+    motionObject.style.animationPlayState = 'paused'; // Pause the animation
+});
